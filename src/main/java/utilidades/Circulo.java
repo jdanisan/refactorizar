65aed93e2cfc180df0a3d1dfe5c1b4da46b2d708
JD <jangu2464@gmail.com>
2024-03-05 21:16:54 +0100

    Integración archivos Cesar

--- src/main/java/utilidades/Circulo.java
+++ src/main/java/utilidades/Circulo.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package utilidades;
 
-/**
- *
- * @author Vespertino
- */
 public class Circulo {
-    
-}
+	private double rad;
+
+	public Circulo(double radio)
+	    	{
+	        this.rad = radio;
+	}
+
+	    	public void imprimir()
+	    	{
+	        String color = "rojo";
+	        System.out.println("Di�metro: " + 2*rad);
+	        System.out.println("Color: " + color);
+	        double area = 2 * 3.1416 * rad * rad;
+	        System.out.println(area);
+	}
+
+	public boolean esIgual (Circulo otro, boolean conDecimales)
+	{
+	        double radio1 = this.rad;
+	        double radio2 = otro.getRad();
+	        if (conDecimales)
+	        {
+	            if (radio1 == radio2) 
+	              return true;
+	            else
+	              return false;
+	        }
+	        else
+	        {
+	            if (Math.abs(radio1-radio2)<1)
+	                return true;
+	            else
+	                return false;
+	        }
+	    }
+
+    private double getRad() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+	}
+
